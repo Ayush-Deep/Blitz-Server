7b27507ed820047302249ce9d8913be81e1802cd
Pranav chaudhari <76875882+snowman1711@users.noreply.github.com>
2021-01-20 15:03:35 +0530

    Update ChatText.py

--- BombSquad-Mods/ChatText.py
+++ BombSquad-Mods/ChatText.py
@@ -1,6 +1,13 @@
+#To use this you need settings.py from this repo
+#else if you have settings.py 
+#then put following there without #
+#messageList=['pranav','lol','me','egg','egg009']
+#chatMessageTime=50000 #5000 means 50 sec don't change me recomend :)
+
 #this file mads by Bltiz Modders
 #credit if you want :) 
 #No, altlist don't remove this 3 lines
+
 import bs,bsInternal
 import random
 from settings import *
