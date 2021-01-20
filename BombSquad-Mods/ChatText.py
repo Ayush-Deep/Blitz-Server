776dbeb69357a2cba5111276bc73a24c45df7a91
Ayush-Deep <77712498+Ayush-Deep@users.noreply.github.com>
2021-01-20 22:46:33 +0530

    Add files via upload

---
+++ BombSquad-Mods/ChatText.py
@@ -0,0 +1,13 @@
+import bs,bsInternal
+import random
+from settings import *
+inx = 0
+def message():
+    global inx
+    bs.screenMessage(messageList[inx],color = msgColor)
+    if inx < len(messageList):
+        inx+= 1
+    if inx == len(messageList):
+        inx = 0
+CMT = chatMessageTime*1000
+timer = bs.Timer(CMT,message,timeType='real',repeat=True)
